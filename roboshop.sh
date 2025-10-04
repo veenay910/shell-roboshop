4abb7342fffb713b6ac1d4147485a8355df98b77
veenay910 <veenay910@gmail.com>
2025-10-04 23:15:50 +0530

    instance

---
+++ roboshop.sh
@@ -0,0 +1,18 @@
+AMI_ID=ami-09c813fb71547fc4f
+SG_ID=sg-03fe2ffe4f5201850
+
+for instance in $@
+do
+    INSTANCE_ID=(aws ec2 run-instances  --image-id $AMI_ID  --instance-type t3.micro  --security-group-ids $SG_ID --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=$instance}]' --query 'Instances[0].InstanceId' --output text)
+    if [ $instance != frontend ]; then
+        aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
+    else
+        aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PubliceIpAddress' --output text
+    fi
+
+done
+
+
+
+
+
