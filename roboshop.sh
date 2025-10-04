f826fd830dd3a32b270a08cfee2ce75564279bbf
veenay910 <veenay910@gmail.com>
2025-10-04 23:37:55 +0530

    instance

--- roboshop.sh
+++ roboshop.sh
@@ -3,7 +3,7 @@ SG_ID=sg-03fe2ffe4f5201850
 
 for instance in $@
 do
-    INSTANCE_ID=(aws ec2 run-instances  --image-id $AMI_ID  --instance-type t3.micro  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
+    INSTANCE_ID=(aws ec2 run-instances --image-id $AMI_ID --instance-type t3.micro  --security-group-ids $SG_ID --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" --query 'Instances[0].InstanceId' --output text)
     if [ $instance != frontend ]; then
         aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text
     else
