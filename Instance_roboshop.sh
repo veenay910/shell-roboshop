acdce739a504a9dcf4ceeccdb0ec445e04e51320
veenay910 <veenay910@gmail.com>
2025-10-07 21:39:19 +0530

    instance

--- Instance_roboshop.sh
+++ Instance_roboshop.sh
@@ -13,7 +13,7 @@ do
         IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PrivateIpAddress' --output text)
         RECORD_NAME=$instance.$DOMAIN_NAME
     else
-        IP=(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
+        IP=$(aws ec2 describe-instances --instance-ids $INSTANCE_ID --query 'Reservations[0].Instances[0].PublicIpAddress' --output text)
         RECORD_NAME=$DOMAIN_NAME
 
     fi
