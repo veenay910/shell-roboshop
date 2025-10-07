5e598410717640507c73277626e9a660cafa06dd
veenay910 <veenay910@gmail.com>
2025-10-07 21:12:32 +0530

    instance

---
+++ redis.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+
+# Define color variables for clarity
+R='\e[31m'
+G='\e[32m'
+B='\e[34m'
+Y='\e[33m'
+N='\e[0m'
+
+LOG_FOLDER="/var/log/shell-mongo"
+SCRIPT_FILE=$( echo $0 | cut -d "." -f1 )
+LOG_FILE=$LOG_FOLDER/$SCRIPT_FILE.log
+
+USERID=$(id -u)
+
+mkdir -p $LOG_FOLDER
+
+if [ $USERID -ne 0 ]; then
+    echo -e "Run script with Sudo permissins...  $R Validatin Failed $N " | tee -a $LOG_FILE
+    exit 1
+else
+    echo -e "SUdo permissions validated...   $G Validatin Success $N " | tee -a $LOG_FILE
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+    echo -e "$2 $R Failuer $N" | tee -a $LOG_FILE
+    exit 1  
+    else
+    echo -e "$2 $G success $N" | tee -a $LOG_FILE
+
+    fi
+}
+
+dnf module disable redis -y
+VALIDATE $? "Disable existing redis" 
+
+dnf module enable redis:7 -y
+VALIDATE $? "enable redis:7" 
+
+dnf install redis -y 
+VALIDATE $? "install redis" 
+
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/redis/redis.conf
+VALIDATE $? "updated to 0.0.0.0" 
+
+# protected-mode from yes to no in /etc/redis/redis.conf
+# vim /etc/redis/redis.conf
+
+systemctl enable redis 
+VALIDATE $? "enable redis" 
+
+systemctl start redis 
+VALIDATE $? "start redis" 
