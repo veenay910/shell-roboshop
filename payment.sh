e92e3af3908058f512bd1aba81861bc8c5c468cc
veenay910 <veenay910@gmail.com>
2025-10-09 15:47:47 +0530

    instance

--- payment.sh
+++ payment.sh
@@ -1,73 +1,138 @@
+# #!/bin/bash
+
+# # Define color variables for clarity
+# R='\e[31m'
+# G='\e[32m'
+# B='\e[34m'
+# Y='\e[33m'
+# N='\e[0m'
+
+# LOG_FOLDER="/var/log/shell-mongo"
+# SCRIPT_FILE=$( echo $0 | cut -d "." -f1 )
+# LOG_FILE=$LOG_FOLDER/$SCRIPT_FILE.log
+# SERVICE_FILE=$PWD
+# mkdir -p $LOG_FOLDER
+
+# USERID=$(id -u)
+
+# if [ $USERID -ne 0 ]; then
+#     echo -e "Run script with Sudo permissins...  $R Validatin Failed $N " | tee -a $LOG_FILE
+#     exit 1
+# else
+#     echo -e "SUdo permissions validated...   $G Validatin Success $N " | tee -a $LOG_FILE
+# fi
+
+
+# VALIDATE(){
+#     if [ $1 -ne 0 ]; then
+#     echo -e "$2 $R Failuer $N" | tee -a $LOG_FILE
+#     exit 1  
+#     else
+#     echo -e "$2 $G success $N" | tee -a $LOG_FILE
+
+#     fi
+# }
+# dnf install python3 gcc python3-devel -y &>>$LOG_FILE
+# VALIDATE $? "Install python"
+
+# id roboshop
+# if [ $? -ne 0 ]; then
+#     echo "Crete roboshp user"
+#     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+# else
+#     echo "User already exist"
+# fi
+# VALIDATE $? "add system user"
+
+# mkdir -p /app &>>$LOG_FILE
+# VALIDATE $? "creating app directory"
+
+# curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+# VALIDATE $? "downloading code"
+
+# cd /app &>>$LOG_FILE
+# VALIDATE $? "switching to add directory"
+
+# rm -rf /app/*
+# VALIDATE $? "removing existing code"
+
+# unzip /tmp/payment.zip &>>$LOG_FILE
+# VALIDATE $? "unzipping code"
+
+# pip3 install -r requirements.txt &>>$LOG_FILE
+# VALIDATE $? "install dependencies"
+
+# cp $SERVICE_FILE/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
+# VALIDATE $? "coping service file"
+
+# systemctl daemon-reload
+# systemctl enable payment &>>$LOG_FILE
+# systemctl restart payment &>>$LOG_FILE
+# VALIDATE $? "enable and restart payment"
+
 #!/bin/bash
 
-# Define color variables for clarity
-R='\e[31m'
-G='\e[32m'
-B='\e[34m'
-Y='\e[33m'
-N='\e[0m'
-
-LOG_FOLDER="/var/log/shell-mongo"
-SCRIPT_FILE=$( echo $0 | cut -d "." -f1 )
-LOG_FILE=$LOG_FOLDER/$SCRIPT_FILE.log
-SERVICE_FILE=$PWD
-mkdir -p $LOG_FOLDER
-
 USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+LOGS_FOLDER="/var/log/shell-roboshop"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+SCRIPT_DIR=$PWD
+MONGODB_HOST=mongodb.ddaws86s.fun
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" # /var/log/shell-script/16-logs.log
+MYSQL_HOST=mysql.ddaws86s.fun
+
+mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
-    echo -e "Run script with Sudo permissins...  $R Validatin Failed $N " | tee -a $LOG_FILE
-    exit 1
-else
-    echo -e "SUdo permissions validated...   $G Validatin Success $N " | tee -a $LOG_FILE
+    echo "ERROR:: Please run this script with root privelege"
+    exit 1 # failure is other than 0
 fi
 
-
-VALIDATE(){
+VALIDATE(){ # functions receive inputs through args just like shell script args
     if [ $1 -ne 0 ]; then
-    echo -e "$2 $R Failuer $N" | tee -a $LOG_FILE
-    exit 1  
+        echo -e "$2 ... $R FAILURE $N" | tee -a $LOG_FILE
+        exit 1
     else
-    echo -e "$2 $G success $N" | tee -a $LOG_FILE
-
+        echo -e "$2 ... $G SUCCESS $N" | tee -a $LOG_FILE
     fi
 }
+
 dnf install python3 gcc python3-devel -y &>>$LOG_FILE
-VALIDATE $? "Install python"
 
-id roboshop
+id roboshop &>>$LOG_FILE
 if [ $? -ne 0 ]; then
-    echo "Crete roboshp user"
     useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOG_FILE
+    VALIDATE $? "Creating system user"
 else
-    echo "User already exist"
+    echo -e "User already exist ... $Y SKIPPING $N"
 fi
-VALIDATE $? "add system user"
 
-mkdir -p /app &>>$LOG_FILE
-VALIDATE $? "creating app directory"
+mkdir -p /app
+VALIDATE $? "Creating app directory"
 
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
-VALIDATE $? "downloading code"
+curl -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment-v3.zip &>>$LOG_FILE
+VALIDATE $? "Downloading payment application"
 
-cd /app &>>$LOG_FILE
-VALIDATE $? "switching to add directory"
+cd /app 
+VALIDATE $? "Changing to app directory"
 
 rm -rf /app/*
-VALIDATE $? "removing existing code"
+VALIDATE $? "Removing existing code"
 
 unzip /tmp/payment.zip &>>$LOG_FILE
-VALIDATE $? "unzipping code"
+VALIDATE $? "unzip payment"
 
 pip3 install -r requirements.txt &>>$LOG_FILE
-VALIDATE $? "install dependencies"
 
-cp $SERVICE_FILE/payment.service /etc/systemd/system/payment.service &>>$LOG_FILE
-VALIDATE $? "coping service file"
-
+cp $SCRIPT_DIR/payment.service /etc/systemd/system/payment.service
 systemctl daemon-reload
-systemctl enable payment &>>$LOG_FILE
-systemctl restart payment &>>$LOG_FILE
-VALIDATE $? "enable and restart payment"
+systemctl enable payment  &>>$LOG_FILE
+
+systemctl restart payment
 
 
